0d32e2c88a6c577af2ff317b2252ceec4ee895b1
Jitendra <jeetupyadav1988@gmail.com>
2024-03-16 06:51:46 +0530

    datadriven Test repository

---
+++ src/test/java/testcases/UserRegTest.java
@@ -0,0 +1,22 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+import utilities.DataUtil;
+
+public class UserRegTest extends BaseTest {
+
+	
+	@Test(dataProviderClass = DataUtil.class, dataProvider = "dp")
+	public void registerUser(String firstName, String lastName) {
+		
+		System.out.println(firstName+"---"+lastName);
+		
+	}
+	
+	
+
+
+	
+}
